4efed03a92acedd0ef9871307c0f0cb3e30a5ef6
Russell Oyama <russelloyama@gmail.com>
2021-09-29 07:42:24 -1000

    coroutine to move enemy

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -6,18 +6,20 @@ public class EnemyMover : MonoBehaviour
 {
 
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1f;
 
     // Start is called before the first frame update
     void Start()
-    {
-        PrintWaypointName();
+    {        
+        StartCoroutine(FollowPath());    
     }
 
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
