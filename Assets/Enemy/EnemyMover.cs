3ef3b15b56607e749cc06c1a24b3f6120fec0a90
Russell Oyama <russelloyama@gmail.com>
2021-10-11 07:43:03 -1000

    find the path

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -10,8 +10,27 @@ public class EnemyMover : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {        
+    {
+        FindPath();
+        ReturnToStart();
         StartCoroutine(FollowPath());    
+    }
+
+    void FindPath()
+    {
+        path.Clear();
+
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Path");
+
+        foreach(GameObject waypoint in waypoints)
+        {
+            path.Add(waypoint.GetComponent<Waypoint>());
+        }
+    }
+
+    void ReturnToStart()
+    {
+        transform.position = path[0].transform.position;
     }
 
     IEnumerator FollowPath()
@@ -32,5 +51,6 @@ public class EnemyMover : MonoBehaviour
             }
             
         }
+        Destroy(gameObject);
     }
 }
