cef13cdab3d128ad5a524327c106fa399261c5eb
Russell Oyama <russelloyama@gmail.com>
2021-10-14 07:06:13 -1000

    attacking closest enemy

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -6,22 +6,55 @@ public class TargetLocator : MonoBehaviour
 {
 
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        target = closestTarget;
     }
 
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
         weapon.LookAt(target);
+
+        if(targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
