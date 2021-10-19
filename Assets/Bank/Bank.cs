4bfdadd5a602d85442caf2c615c1cc9239e017de
Russell Oyama <russelloyama@gmail.com>
2021-10-19 07:31:20 -1000

    creating UI canvas

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
 
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     private void Awake()
     {
         currentBalance = startingBalance;
@@ -19,17 +22,24 @@ public class Bank : MonoBehaviour
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
-        if(currentBalance < 0)
+        if (currentBalance < 0)
         {
             //Lose the game;
             ReloadScene();
         }
+    }
+    
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
 
