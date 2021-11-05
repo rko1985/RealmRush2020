97dfab19dfe9cfb28ca7d7c645b8c357ea300072
Russell Oyama <russelloyama@gmail.com>
2021-11-05 08:19:33 -1000

    checking if the path is valid

--- Assets/Environment/Tile.cs
+++ Assets/Environment/Tile.cs
@@ -10,11 +10,13 @@ public class Tile : MonoBehaviour
     public bool IsPlaceable {get{return isPlaceable;}}
 
     GridManager gridManager;
+    Pathfinder pathfinder;
     Vector2Int coordinates = new Vector2Int();
 
     void Awake()
     {
         gridManager = FindObjectOfType<GridManager>();
+        pathfinder = FindObjectOfType<Pathfinder>();
     }
     void Start()
     {
@@ -30,10 +32,11 @@ public class Tile : MonoBehaviour
 
     void OnMouseDown()
     {
-        if(isPlaceable)
+        if (gridManager.GetNode(coordinates).isWalkable && !pathfinder.WillBlockPath(coordinates)) 
         {
             bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);            
             isPlaceable = !isPlaced;
+            gridManager.BlockNode(coordinates);
         }
     }
 }
